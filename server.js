7a16141605635b7e2b143f61a827de7495be020d
yash <yashkapadiya191@gmail.com>
2025-06-28 10:19:32 +0530

    changes

--- server.js
+++ server.js
@@ -8,7 +8,8 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '5mb' }));
+app.use(express.urlencoded({ limit: '5mb', extended: true }));
 
 // DB Connection
 mongoose.connect(process.env.MONGODB_URI, {
